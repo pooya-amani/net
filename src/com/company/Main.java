333f2e0da02565a569877f972c911e2ae74869a5
pooya-amani <pooya.amani2020@gmail.com>
2020-04-28 18:26:05 +0430

    commit this file

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int x = 10;
+        System.out.println(x);
+
+
+        float z =32;
+        System.out.println(z);
+
+        String c = "pooya";
+        System.out.println(c.length());
+    }
+
+
+}
